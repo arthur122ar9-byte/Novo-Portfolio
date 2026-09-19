aad0d703c3a2f8a022ff4a1d3bd31bf738b1166f
arthur122ar9-byte <arthur122ar9@gmail.com>
2026-09-19 11:49:23 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const enviar = document.getElementById('enviar')
+
+enviar.addEventListener('click', () => {
+
+    const nome = document.getElementById('nome').value.trim()
+    const email = document.getElementById('email').value.trim()
+    const telefone = document.getElementById('telefone').value.trim()
+    const msg = document.getElementById('msg').value.trim()
+    if (nome != '' && email != '' && telefone != '' && msg != '') {
+        const meuNumero = '5511917770051'
+
+        const textMsg =
+            `*Nova mensagem recebida pelo site*
+*Nome:* ${nome}
+*E-mail:* ${email}
+*Telefone:* ${telefone}
+*Mensagem:*
+${msg}`
+
+        const codText = encodeURIComponent(textMsg)
+        const waweb = `https://web.whatsapp.com/send?phone=${meuNumero}&text=${codText}`
+        window.open(waweb, '_blank')
+    } else {
+        window.alert('Por favor, preencha o formulário para enviar uma mensagem!')
+    }
+})
